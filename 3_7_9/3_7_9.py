42fa0bffbec52a694b37181b09799deb9276320f
waigoma <48357625+waigoma@users.noreply.github.com>
2023-10-13 11:57:55 +0900

    update

--- 3_7_9/3_7_9.py
+++ 3_7_9/3_7_9.py
@@ -3,6 +3,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import statsmodels.formula.api as smf
 from statsmodels.stats.anova import anova_lm
+import statsmodels.api as sm
 
 # pandas の表示オプション
 pd.set_option('display.max_columns', 100)  # 表示する列数の設定
@@ -18,21 +19,23 @@ Auto = Auto.dropna()  # 欠損値を含む行を削除
 
 # (b) cor() 関数を使い、相関行列を計算する。変数 name は相関行列に含めないこと。
 correlation_matrix = Auto.drop(columns='name').corr()
-print(correlation_matrix)
+# print(correlation_matrix)
 
 # (c) lm() 関数を使い、mpg を応答変数、name 以外のすべてを予測変数とする線形重回帰を当てはめる。
 # このとき、summary() 関数を使い、当てはまったモデルの結果を表示する。
-# 線形回帰モデルの作成
-formula = "mpg ~ cylinders + displacement + horsepower + weight + acceleration + year + origin"
-lm_fit1 = smf.ols(formula=formula, data=Auto).fit()
-# モデルの summary を表示
-print(lm_fit1.summary())
+
+# 予測変数と応答変数を設定
+X = Auto.drop(['mpg', 'name'], axis=1)
+y = Auto['mpg']
+
+# 定数項（切片）をモデルに追加
+X = sm.add_constant(X)
+
+# 線形回帰モデルを作成
+model = sm.OLS(y, X).fit()
 
 # i. 予測変数と応答変数に関係はあるか
-# statsmodels の anova_lm() 関数を使い、このモデルの分散分析表を表示する。
-# 分散分析を実行
-anova_table = anova_lm(lm_fit1)
-print(anova_table)
+print(model.summary())
 
 # ii. 予測変数のうちどれが応答変数と統計的に優位な関係にあると思われるか
 #
--- 4_4/4_4_graph.py
+++ 4_4/4_4_graph.py
@@ -8,6 +8,7 @@ range_end = 5
 myu1 = -1.25
 myu2 = 1.25
 sigma = 1
+sigma_4 = 4
 
 
 # 確率密度関数
@@ -17,7 +18,7 @@ def pdf():
 
     # 確率密度関数
     y1 = norm.pdf(x, myu1, sigma)
-    y2 = norm.pdf(x, myu2, sigma)
+    y2 = norm.pdf(x, myu2, sigma_4)
 
     # 確率密度関数のグラフ
     plt.plot(x, y1, label="class1", color="green")
@@ -35,7 +36,7 @@ def pdf():
 def hist():
     # 正規分布に従うランダムデータを 20 個生成
     hist1 = norm.rvs(loc=myu1, scale=sigma, size=20)
-    hist2 = norm.rvs(loc=myu2, scale=sigma, size=20)
+    hist2 = norm.rvs(loc=myu2, scale=sigma_4, size=20)
 
     # データのヒストグラム
     plt.hist(hist1, bins=16, color="green", range=(-4, 4))
@@ -54,5 +55,5 @@ def bayes_decision_line(m1, m2):
     return (m1 + m2) / 2
 
 
-# pdf()
-hist()
+pdf()
+# hist()
